0536acaca45f061fe9b149e67d33cbdd5491f0c1
poyrazavsever <poyrazavsever@hotmail.com>
2025-07-04 11:06:30 +0300

    chore(auth): register JwtStrategy and JwtAuthGuard in AuthModule

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -4,13 +4,22 @@ import { ConfigModule } from '@nestjs/config';
 import { AuthController } from './auth.controller';
 import { AuthService } from './auth.service';
 import { User, UserSchema } from './schemas/user.schema';
+import { JwtModule } from '@nestjs/jwt';
+import { PassportModule } from '@nestjs/passport';
+import { JwtStrategy } from './strategies/jwt.strategy';
 
 @Module({
   imports: [
     ConfigModule,
     MongooseModule.forFeature([{ name: User.name, schema: UserSchema }]),
+    PassportModule,
+    JwtModule.register({
+      secret: 'test-secret-key', // Gerçek uygulamada bu değer .env'den alınmalı
+      signOptions: { expiresIn: '24h' },
+    }),
   ],
   controllers: [AuthController],
-  providers: [AuthService],
+  providers: [AuthService, JwtStrategy],
+  exports: [AuthService],
 })
 export class AuthModule {}
