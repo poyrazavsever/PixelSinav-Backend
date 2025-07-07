005db61007dc722c3caedd62c02d4268f5b43f73
poyrazavsever <poyrazavsever@hotmail.com>
2025-07-07 11:14:24 +0300

    feat(application): added application dto's for validate form

---
+++ src/application/dto/application.dto.ts
@@ -0,0 +1,46 @@
+import {
+  IsEmail,
+  IsNotEmpty,
+  MaxLength,
+  MinLength,
+  IsString,
+} from 'class-validator';
+
+export class ApplicationDto {
+  @IsNotEmpty({ message: 'İsim Alanı Boş Olamaz' })
+  @IsString({ message: 'İsim Alanı Geçersiz' })
+  name: string;
+
+  @IsNotEmpty({ message: 'E-posta Alanı Boş Olamaz' })
+  @IsEmail({}, { message: 'Geçersiz E-posta Adresi' })
+  email: string;
+
+  @IsNotEmpty({ message: 'Telefon Alanı Boş Olamaz' })
+  phone: string;
+
+  @IsNotEmpty()
+  @IsString()
+  @MinLength(8, { message: 'Eğitim Bilgisi En Az 8 Karakter Olmalı' })
+  @MaxLength(64, { message: 'Eğitim Bilgisi En Fazla 64 Karakter Olmalı' })
+  eduInformation?: string;
+
+  @IsNotEmpty()
+  @IsString()
+  @MinLength(8, { message: 'Deneyim Bilgisi En Az 8 Karakter Olmalı' })
+  @MaxLength(600, { message: 'Deneyim Bilgisi En Fazla 600 Karakter Olmalı' })
+  experience?: string;
+
+  @IsNotEmpty()
+  @IsString()
+  @MinLength(8, { message: 'Uzmanlık Bilgisi En Az 8 Karakter Olmalı' })
+  @MaxLength(600, { message: 'Uzmanlık Bilgisi En Fazla 600 Karakter Olmalı' })
+  expertise?: string;
+
+  @IsNotEmpty()
+  @IsString()
+  resume?: string;
+
+  @IsNotEmpty()
+  @IsString()
+  certificates?: string[];
+}
