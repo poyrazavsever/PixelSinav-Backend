b577aea46b30d58055ef4c601f5920eb6ba7cf77
poyrazavsever <poyrazavsever@hotmail.com>
2025-07-07 14:27:20 +0300

    feat: add mongoose module configuration

--- src/application/application.module.ts
+++ src/application/application.module.ts
@@ -1,10 +1,17 @@
 import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
 import { ApplicationController } from './application.controller';
 import { ApplicationService } from './application.service';
+import { TeacherApplication, UserSchema } from './schemas/application.schema';
 
 @Module({
+  imports: [
+    MongooseModule.forFeature([
+      { name: TeacherApplication.name, schema: UserSchema },
+    ]),
+  ],
   controllers: [ApplicationController],
   providers: [ApplicationService],
-  exports: [ApplicationService], // Diğer modüllerde kullanılabilmesi için export ediyoruz
+  exports: [ApplicationService],
 })
 export class ApplicationModule {}
