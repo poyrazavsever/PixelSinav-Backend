c712c121976ccd8d9f2eea5e8304eb31267a61cc
poyrazavsever <poyrazavsever@hotmail.com>
2025-07-08 10:29:38 +0300

    fix: add type safety to RolesGuard

---
+++ src/lessons/guards/auth.guard.ts
@@ -0,0 +1,33 @@
+import {
+  CanActivate,
+  ExecutionContext,
+  Injectable,
+  UnauthorizedException,
+} from '@nestjs/common';
+import { Reflector } from '@nestjs/core';
+import { RequestWithUser } from '../interfaces/request-with-user.interface';
+
+@Injectable()
+export class RolesGuard implements CanActivate {
+  constructor(private reflector: Reflector) {}
+
+  canActivate(context: ExecutionContext): boolean {
+    const roles = this.reflector.get<string[]>('roles', context.getHandler());
+    if (!roles) {
+      return true;
+    }
+
+    const request = context.switchToHttp().getRequest<RequestWithUser>();
+    const user = request.user;
+
+    if (!user) {
+      throw new UnauthorizedException('Kullanıcı bulunamadı');
+    }
+
+    return this.matchRoles(roles, user.roles);
+  }
+
+  private matchRoles(roles: string[], userRoles: string[]): boolean {
+    return roles.some((role) => userRoles.includes(role));
+  }
+}
